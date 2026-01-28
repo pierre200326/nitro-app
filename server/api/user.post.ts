65766ade0ee30d70d75b39c924cde249f10b2dbe
pierre200326 <nichelepierre@gmail.com>
2026-01-28 11:21:40 +0100

    post user

--- server/api/user.post.ts
+++ server/api/user.post.ts
@@ -1,13 +1,19 @@
 import { pool } from "../db";
-import { defineEventHandler, readBody } from "h3";
+import { createError, defineEventHandler, readBody } from "h3";
 
 export default defineEventHandler(async (event) => {
-    const { pseudo, mdp } = await readBody(event);
+    try {
+        const body = await readBody(event);
+        const { pseudo, mdp } = body;
 
-    const result = await pool.query(
-        "INSERT INTO users (pseudo, mdp) VALUES ($1, $2) RETURNING id, pseudo, best_score",
-        [pseudo, mdp]
-    );
+        const result = await pool.query(
+            "INSERT INTO users (pseudo, mdp) VALUES ($1, $2) RETURNING id, pseudo, best_score",
+            [pseudo, mdp]
+        );
 
-    return result.rows[0];
+        return result.rows[0];
+    } catch (err) {
+        console.error('Erreur API /user :', err);
+        throw createError({ statusCode: 500, statusMessage: "Server Error" });
+    }
 });
