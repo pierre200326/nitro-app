25f762f0b38b151ae5395b352c2b39feaf2fec5c
pierre200326 <nichelepierre@gmail.com>
2026-01-28 11:50:50 +0100

    modif

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,16 @@ import { defineNitroConfig } from "nitropack/config"
 
 // https://nitro.build/config
 export default defineNitroConfig({
+  routeRules: {
+    // Ceci intercepte la requête OPTIONS avant qu'elle n'arrive à ton fichier .post
+    '/api/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      }
+    },
+  },
   compatibilityDate: "latest",
   srcDir: "server",
   imports: false
