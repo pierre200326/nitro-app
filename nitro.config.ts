4b9d55c6efaf7f25d6b0805dc1617852413d76b1
AntoCu <antonin.cuyala@gmail.com>
2026-02-09 22:53:34 +0100

    Update nitro.config.ts
    Ajout d'une route proxy

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,10 @@ import { defineNitroConfig } from "nitropack/config";
 
 export default defineNitroConfig({
   routeRules: {
+    // 1. On crée le "pont" vers ton autre API Vercel
+    '/api-game/**': { 
+      proxy: 'https://nitro-app-bice.vercel.app/api/**' 
+    },
     '/api/*': {
       cors: true,
     }
